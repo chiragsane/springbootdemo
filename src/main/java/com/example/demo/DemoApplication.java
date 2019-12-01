1647df4a580435771971abebe318c4cc08f2bc0b
Admin <Admin@DESKTOP-CSMO4CR>
2019-12-02 00:40:53 +0530

    commit1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("testig testong");
+		System.out.println("thi is incorrect commt");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
